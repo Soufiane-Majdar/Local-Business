cd493cbb37a37a80f55a24de69ffc0292f99f90b
Sσϝιαɳ <soufianemjr3145@gmail.com>
2022-10-01 17:59:04 +0100

    new commit

--- Dev_Business/urls.py
+++ Dev_Business/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -23,3 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Main.urls')),
 ]
+urlpatterns  +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
