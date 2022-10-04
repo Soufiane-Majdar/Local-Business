b9c1be3ae2f0799dcf43e90ced5fbd64d518fc2e
Soufiane-Majdar <soufianemjr3145@gmail.com>
2022-10-04 18:15:10 +0100

    new commit

--- Main/urls.py
+++ Main/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ 
-    path('', views.home, name='home'), 
+urlpatterns = [
+    path('', views.home, name='home'),
     path('detail/<int:id>', views.detail, name='detail'),
-    path('favorite/<int:id>',views.favorite,name='favorite'),
-    path('upload_csv',views.upload_csv, name='upload_csv'),
+    path('favorite/<int:id>', views.favorite, name='favorite'),
+    path('upload_csv', views.upload_csv, name='upload_csv'),
     # static change by form and busines id
-    path('status/<int:id>',views.status,name='status'),
+    path('status/<int:id>', views.status, name='status'),
     # login
-    path('login',views.login_user,name='login'),
+    path('login', views.login_user, name='login'),
     # logoout
-    path('logout',views.logout_user,name='logout'),
+    path('logout', views.logout_user, name='logout'),
+    # contact
+    path('contact', views.contact, name='contact'),
+
+
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,10 +1,11 @@
 from distutils.log import error
 from multiprocessing import context
 from django.shortcuts import render, redirect, HttpResponseRedirect
-from .models import Busines,Favorite
+from .models import Busines, Favorite
+
 # import messages
 # Create your views here.
-import  csv
+import csv
 import pandas as pd
 import os
 
@@ -12,17 +13,25 @@ import json
 # csv_file= os.path.join(os.path.dirname(__file__), 'results.csv')
 
 # import user authentification django
-from django.contrib.auth import authenticate , login,logout
+from django.contrib.auth import authenticate, login, logout
 # create a function login and create session
+
+# 404 page
+
+
+def page_not_found_view(request, exception):
+    return render(request, '404_page.html', status=404)
+
+
 def login_user(request):
     # if session is not created
-    if  request.session.has_key('USER'):
+    if request.session.has_key('USER'):
         # redirect to login page
         return redirect('home')
     # if get method
     if request.method == 'GET':
         # redirect to login page
-        return render(request, 'login.html',{'title': 'Login'})
+        return render(request, 'login.html', {'title': 'Login'})
     # if post method
     if request.method == 'POST':
         # get the username and password from the form
@@ -39,19 +48,22 @@ def login_user(request):
             # redirect to home page
             return redirect('home')
         else:
-            error="Username or Password is incorrect"
+            error = "Username or Password is incorrect"
             # redirect to login page
-            return render(request, 'login.html',{'title': 'Login','error':error})
+            return render(request, 'login.html', {'title': 'Login', 'error': error})
 
 # create a function logout and delete session
+
+
 def logout_user(request):
     # delete session
     del request.session['USER']
     request.session.modified = True
-    #logout
+    # logout
     logout(request)
     # redirect to login page
     return redirect('login')
+
 
 def home(request):
     # if session is not created
@@ -63,7 +75,7 @@ def home(request):
     business = Busines.objects.all()
     # cout the number of business
     nbr_business = business.count()
-    # count business by status and stor values in a json 
+    # count business by status and stor values in a json
     new = Busines.objects.filter(status=0).count()
     onhold = Busines.objects.filter(status=1).count()
     inprogress = Busines.objects.filter(status=2).count()
@@ -73,7 +85,7 @@ def home(request):
     accepted = Busines.objects.filter(status=6).count()
     noresponse = Busines.objects.filter(status=7).count()
     # create a json
-    status =""
+    status = ""
     if new > 0:
         status += "[ {value:"+str(new)+",name:'New'}"
     if inprogress > 0:
@@ -91,15 +103,16 @@ def home(request):
     if noresponse > 0:
         status += ",{value:"+str(noresponse)+",name:'No Response'}]"
 
-
     # convert json to string
-    status="<script> document.addEventListener('DOMContentLoaded', () => { echarts.init(document.querySelector('#trafficChart')).setOption({ tooltip: {  trigger: 'item' }, legend: {top: '5%',left: 'center'}, series: [{name: 'Statu :', type: 'pie',radius: ['40%', '70%'], avoidLabelOverlap: false,label: {  show: false,   position: 'center' },emphasis: { label: { show: true,fontSize: '18',fontWeight: 'bold'}},labelLine: {show: false}, data: "+status+"}]});}); </script>"
-    
+    status = "<script> document.addEventListener('DOMContentLoaded', () => { echarts.init(document.querySelector('#trafficChart')).setOption({ tooltip: {  trigger: 'item' }, legend: {top: '5%',left: 'center'}, series: [{name: 'Statu :', type: 'pie',radius: ['40%', '70%'], avoidLabelOverlap: false,label: {  show: false,   position: 'center' },emphasis: { label: { show: true,fontSize: '18',fontWeight: 'bold'}},labelLine: {show: false}, data: "+status+"}]});}); </script>"
+
     favorites = Favorite.objects.all()
     nbr_favorite = favorites.count()
 
-    context = {'title': 'Home','business':business,'status':status,'favorites':favorites,'nbr_business':nbr_business,'nbr_favorite':nbr_favorite}
+    context = {'title': 'Home', 'business': business, 'status': status,
+               'favorites': favorites, 'nbr_business': nbr_business, 'nbr_favorite': nbr_favorite}
     return render(request, 'home.html', context)
+
 
 def detail(request, id):
     # if session is not created
@@ -111,31 +124,33 @@ def detail(request, id):
     # get the next and the previous business
     next = Busines.objects.filter(id__gt=id).order_by('id').first()
     previous = Busines.objects.filter(id__lt=id).order_by('-id').first()
-    pagination={}
+    pagination = {}
     if previous:
-        pagination['previous']=previous.id
+        pagination['previous'] = previous.id
     if next:
-        pagination['next']=next.id
+        pagination['next'] = next.id
 
-    context = {'title': 'Detail','busines':busines,'favorite':favorite,'pagination':pagination}
+    context = {'title': 'Detail', 'busines': busines,
+               'favorite': favorite, 'pagination': pagination}
     return render(request, 'detail.html', context)
 
 # add to Favorite functions
+
+
 def favorite(request, id):
     # if session is not created
     if not request.session.has_key('USER'):
         # redirect to login page
         return redirect('login')
-    favorite=Favorite()
+    favorite = Favorite()
     # if buisness alredy in Favorite delete it
     if Favorite.objects.filter(busines_id=id).exists():
         Favorite.objects.filter(busines_id=id).delete()
     else:
         # add to Favorite
-        favorite.busines_id=id
+        favorite.busines_id = id
         favorite.save()
     return redirect('detail', id=id)
-
 
 
 def upload_csv(request):
@@ -146,21 +161,22 @@ def upload_csv(request):
     try:
         if request.method != 'POST':
             context = {'title': 'Add Business'}
-            return render(request, 'upload_csv.html',context)
+            return render(request, 'upload_csv.html', context)
         csv_file = request.FILES["formFile"]
         print(csv_file)
         print(csv_file)
         if not csv_file.name.endswith('.csv'):
-            error='File is not CSV type'
-            context = {'title': 'Add Business | Error','error':error}
+            error = 'File is not CSV type'
+            context = {'title': 'Add Business | Error', 'error': error}
             return render(request, 'upload_csv.html', context)
         # if file is too large, return
         if csv_file.multiple_chunks():
-            error='Uploaded file is too big (%.2f MB).' % (csv_file.size/(1000*1000),)
-            context = {'title': 'Add Business | Error','error':error}
+            error = 'Uploaded file is too big (%.2f MB).' % (
+                csv_file.size/(1000*1000),)
+            context = {'title': 'Add Business | Error', 'error': error}
             return render(request, 'upload_csv.html', context)
-            
-        #fill Busines class from csv file 
+
+        # fill Busines class from csv file
         df = pd.read_csv(csv_file)
         # "Name","Fulladdress",Street,Municipality,Categories,Phone,"Phones",Review Count,Average Rating,"Review URL",Google Maps URL,Latitude,Longitude,Website,Domain,"Opening hours",Featured image
         for index, row in df.iterrows():
@@ -187,59 +203,17 @@ def upload_csv(request):
             except:
                 pass
 
-        # fille_data = csv_file.read().decode('UTF-8')
-        # print(fille_data)
-        # lines = fille_data.split("\n")
-        
-        # # loop over the lines and save them in db. If error , store as string and then display  
-        # for line in lines:
-        #     # scipe the first line
-        #     # if line.startswith('Name'):
-        #     #     continue
-        #     row = line.split(",")
-        #     # start from the second line
-        #     if 'Name' not in row[0] :
-        #         try:
-        #                 print(row)
-        #                 # create object
-        #                 business = Busines()
-        #                 # fill object
-        #                 # remove " from string if exist
-        #                 business.name = row[0].replace('"','')
-        #                 business.fulladdress = row[1].replace('"','')
-        #                 business.street = row[2].replace('"','')
-        #                 business.municipality = row[3].replace('"','')
-        #                 business.categories = row[4].replace('"','')
-        #                 business.phone = row[5].replace('"','')
-        #                 business.phones = row[6].replace('"','')
-        #                 business.review_count = row[7].replace('"','')
-        #                 business.average_rating = row[8].replace('"','')
-        #                 business.review_url = row[9].replace('"','')
-        #                 business.google_maps_url = row[10].replace('"','')
-        #                 business.latitude = row[11].replace('"','')
-        #                 business.longitude = row[12].replace('"','')
-        #                 business.website = row[13].replace('"','')
-        #                 business.domain = row[14].replace('"','')
-        #                 business.opening_hours = row[15].replace('"','')
-        #                 business.featured_image = row[16].replace('"','')
-        #                 # save object
-        #                 business.save()
-                
-        #         except Exception as e:
-        #                 print("Error ", e)
-        #                 # pass to the next row
-        #                 pass
-
-
-        success='File uploaded successfully'
-        context = {'title': 'Add Business | Success','success':success}
+        success = 'File uploaded successfully'
+        context = {'title': 'Add Business | Success', 'success': success}
         return render(request, 'upload_csv.html', context)
     except Exception as e:
-        error='Unable to upload file. '+repr(e)
-        context = {'title': 'Add Business | Error','error':error}
+        error = 'Unable to upload file. '+repr(e)
+        context = {'title': 'Add Business | Error', 'error': error}
         return render(request, 'upload_csv.html', context)
-    
+
 # status function
+
+
 def status(request, id):
     # if session is not created
     if not request.session.has_key('USER'):
@@ -263,41 +237,11 @@ def status(request, id):
         return redirect('detail', id=id)
 
 
-# # function fill Busines class from csv file
-# def fill_business(csv_file):
-#     print("fill_business")
-#     with open(csv_file, 'r') as f:
-#         reader = csv.reader(f)
-#         # skip header
-#         next(reader)
-#         for row in reader:
-#             try:
-#                 print(row)
-#                 # create object
-#                 business = Busines()
-#                 # fill object
-#                 business.name = row[0]
-#                 business.fulladdress = row[1]
-#                 business.street = row[2]
-#                 business.municipality = row[3]
-#                 business.categories = row[4]
-#                 business.phone = row[5]
-#                 business.phones = row[6]
-#                 business.review_count = row[7]
-#                 business.average_rating = row[8]
-#                 business.review_url = row[9]
-#                 business.google_maps_url = row[10]
-#                 business.latitude = row[11]
-#                 business.longitude = row[12]
-#                 business.website = row[13]
-#                 business.domain = row[14]
-#                 business.opening_hours = row[15]
-#                 business.featured_image = row[16]
-#                 # save object
-#                 business.save()
-            
-#             except Exception as e:
-#                 print("Error ", e)
-#                 # pass to the next row
-#                 pass
-
+# Contact functions
+def contact(request):
+    # if session is not created
+    if not request.session.has_key('USER'):
+        # redirect to login page
+        return redirect('login')
+    context = {'title': 'Contact'}
+    return render(request, 'contact.html', context)
